fe7971174784e780b5cac0d020ef8b266f590dd0
Savio Machado <sav@loophole.cc>
2018-09-25 09:03:43 -0300

    build: add config.mk

---
+++ config.mk
@@ -0,0 +1,17 @@
+CC           := clang
+SHELL        := /bin/sh
+EXTRA_CFLAGS := -ggdb3 -O1 -Werror -DDEBUG
+
+KBUILDVERS   ?= $(shell uname -r)
+KBUILDDIR    ?= /lib/modules/$(KBUILDVERS)/build
+
+KSOURCEVER   := 4.18
+KSOURCEPKG   := linux-$(KSOURCEVER).tar.xz
+KSOURCEDIR   := $(KSOURCEPKG:.tar.xz=)
+KSOURCEURL   := https://cdn.kernel.org/pub/linux/kernel/v4.x/$(KSOURCEPKG)
+
+ifneq (,$(wildcard /proc/config.gz))
+KCATCONFIG      := zcat /proc/config.gz
+else
+KCATCONFIG      := cat $(PWD)/.config
+endif
