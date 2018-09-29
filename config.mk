e32e8efc1b19b6e1c755e10f21af33782a04d381
Savio Machado <sav@loophole.cc>
2018-09-29 12:59:20 -0300

    build: fix rules and refactor around a bit

--- config.mk
+++ config.mk
@@ -1,21 +1,29 @@
+# configuration file, feel free to make any changes.
+
 CC           := clang
 SHELL        := /bin/sh
-EXTRA_CFLAGS := -ggdb3 -O1 -Werror -DDEBUG
+
+ifneq (,$(D))
+	EXTRA_CFLAGS := -O1 -Wall -Werr -DDEBUG -ggdb3
+else
+	EXTRA_CFLAGS := -O3 -Wall -Werr -DNDEBUG
+endif
 
 ifneq (4, $(shell uname -r | cut -f1 -d.))
-KBUILDVERS   ?= 4.8
+	KBUILDVERS ?= 4.8
 else
-KBUILDVERS   ?= $(shell uname -r | grep -oE [0-9.]+ | head -1)
+	KBUILDVERS ?= $(shell uname -r | grep -oE [0-9.]+ | head -1)
 endif
-KBUILDDIR    ?= /lib/modules/$(KBUILDVERS)/build
 
+KERNELVERSION = $(KBUILDDIR)
+KBUILDDIR    ?= /lib/modules/$(KBUILDVERS)/build
 KSOURCEVER   := $(KBUILDVERS)
 KSOURCEPKG   := linux-$(KSOURCEVER).tar.xz
 KSOURCEDIR   := $(KSOURCEPKG:.tar.xz=)
 KSOURCEURL   := https://cdn.kernel.org/pub/linux/kernel/v4.x/$(KSOURCEPKG)
 
 ifneq (,$(D))
-KCATCONFIG   := cat $(PWD)/.config.dbg
+	KCATCONFIG := cat $(PWD)/.config.dbg
 else
-KCATCONFIG   := cat $(PWD)/.config
+	KCATCONFIG := cat $(PWD)/.config
 endif
